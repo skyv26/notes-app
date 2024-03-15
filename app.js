ead34c123df7e35556ec1672ded434c2277de67b
Aakash Verma <dev.aakashv@gmail.com>
2024-03-15 17:47:18 +0530

    install the `nodemon` library

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log('Notes Taking App')
+const yargs = require('yargs');
+
+yargs.version('1.0.1');
+
+yargs.command({
+  command: 'add',
+  describe: 'Add a new note',
+  handler: () => {
+    console.log('Adding a new note !');
+  },
+});
