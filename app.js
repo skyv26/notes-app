c8f348b0db375824734c661ccc5b555041026b35
Aakash Verma <dev.aakashv@gmail.com>
2024-03-16 16:51:28 +0530

    `add`: the required arguments and ensure the functionality working

--- app.js
+++ app.js
@@ -7,14 +7,34 @@ const yargs = require('yargs');
 yargs.command({
   command: 'add',
   describe: 'Add a new note',
-  handler: () => {
-    console.log(chalk.greenBright('\nAdding a new note !'));
+  builder: {
+    title: {
+        describe: 'Note title',
+        demandOption: true,
+        type: 'string'
+    },
+    body: {
+        describe: 'Note Body',
+        demandOption: true,
+        type: 'string'
+    }
+  },    
+  handler: (argv) => {
+    console.log(argv.body)
+    console.log(chalk.greenBright('\nAdding a new note !'), argv);
   },
 });
 
 yargs.command({
   command: 'remove',
   describe: 'Remove a note',
+  builder: {
+    title: {
+        describe: 'Note title',
+        demandOption: true,
+        type: 'string'
+    },
+  },
   handler: () => {
     console.log('Removing a new note !');
   },
@@ -23,6 +43,13 @@ yargs.command({
 yargs.command({
   command: 'read',
   describe: 'Read a note',
+  builder: {
+    title: {
+        describe: 'Note title',
+        demandOption: true,
+        type: 'string'
+    },
+  },
   handler: () => {
     console.log('Reading a note !');
   },
@@ -36,4 +63,4 @@ yargs.command({
   },
 });
 
-console.log(yargs.argv);
+yargs.parse()
